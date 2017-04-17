6a7cd848c2a819d1332660075e86300a6e510dcd
Betina-Dalope <betina@bluecadet.com>
2017-04-17 13:24:44 -0400

    Update main.js

--- Difference Engine Simulator/main.js
+++ Difference Engine Simulator/main.js
@@ -1,16 +1,30 @@
+var currentRow = [];
+
 function processInputs() {
   var inputs = $("#inputs :input").serializeArray();
-  console.log(inputs);
 
-  var row1 = {};
-  for (var i = 1; i < inputs.length - 1; i++) {
-    row1[inputs[i].name] = inputs[i].value;
-  } 
+  var row1 = [];
+  for (var i = 1; i < inputs.length; i++) {
+
+    var rawVal = inputs[i].value;
+    var val = (rawVal == "" || rawVal == null || !rawVal ) ? 0 : parseInt(rawVal);
+    row1.push(val);
+  }
+
+  currentRow = row1;
   for (var i = 0; i < inputs[0].value; i++) {
-    calculateNextRow(row1);
+    calculateNextRow();
   }
 }
 
-function calculateNextRow(currentRow) {
-  console.log("calculating row");
+function calculateNextRow() {
+  console.log("calculating row", currentRow);
+  
+  var nextRow = [];
+  currentRow.forEach(function(value, index) {
+    if (index == currentRow.length - 1) nextRow.push(0);
+    else nextRow.push(value + currentRow[index + 1])
+  })
+
+  currentRow = nextRow;
 }
